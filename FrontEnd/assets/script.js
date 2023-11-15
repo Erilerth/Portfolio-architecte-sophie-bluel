f1ce27df6fca0d1027383c117e75f7daf76d16d9
Tony THERLIER <tony.therlier@gmail.com>
2023-11-15 19:05:49 +0100

    feat : added js for filter

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:5678/api/works";
+const API = 'http://localhost:5678/api/works';
 
 async function fetchWorks() {
   const res = await fetch(API);
@@ -7,16 +7,23 @@ async function fetchWorks() {
   return data;
 }
 
-async function displayWorks() {
+async function displayWorks(categoryId = null) {
   const API_dataWorks = await fetchWorks();
 
-  for (let i = 0; i < API_dataWorks.length; i++) {
-    let titleWork = API_dataWorks[i].title;
-    let imgWork = API_dataWorks[i].imageUrl;
+  const filteredWorks = categoryId
+    ? API_dataWorks.filter((work) => work.categoryId === categoryId)
+    : API_dataWorks;
 
-    let workDisplay = document.createElement("figure");
-    let workImg = document.createElement("img");
-    let workTitle = document.createElement("figcaption");
+  const cards = document.querySelector('.gallery');
+  cards.innerHTML = '';
+
+  for (let i = 0; i < filteredWorks.length; i++) {
+    let titleWork = filteredWorks[i].title;
+    let imgWork = filteredWorks[i].imageUrl;
+
+    let workDisplay = document.createElement('figure');
+    let workImg = document.createElement('img');
+    let workTitle = document.createElement('figcaption');
 
     workImg.src = imgWork;
     workImg.alt = titleWork;
@@ -25,8 +32,38 @@ async function displayWorks() {
     workDisplay.appendChild(workImg);
     workDisplay.appendChild(workTitle);
 
-    let cards = document.querySelector(".gallery");
     cards.appendChild(workDisplay);
+  }
+}
+
+document.getElementById('filter').addEventListener('click', handleFilterClick);
+
+function handleFilterClick(event) {
+  const target = event.target;
+
+  if (target.tagName === 'LI') {
+    const selectedCategory = target.textContent.toLowerCase();
+    const categoryId = getCategoryId(selectedCategory);
+
+    document.querySelectorAll('#filter li').forEach((li) => {
+      li.classList.remove('filter-selected');
+    });
+    target.classList.add('filter-selected');
+
+    displayWorks(categoryId);
+  }
+}
+
+function getCategoryId(selectedCategory) {
+  switch (selectedCategory) {
+    case 'objets':
+      return 1;
+    case 'appartements':
+      return 2;
+    case 'hôtels & restaurants':
+      return 3;
+    default:
+      return null;
   }
 }
 
