4a1d2d7021df70a7974ebf8de88cf1c2324111fe
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2023-11-17 16:45:48 +0100

    feat : fully functional filters
    filter buttons are now handled in js and work without new api call each time

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,33 +1,84 @@
-const API = 'http://localhost:5678/api/works';
+const api = 'http://localhost:5678/api/';
+let apiDataWorks = [];
+let apiDataCategories = [];
 
-async function fetchWorks() {
-  const res = await fetch(API);
+async function fetchData() {
+  apiDataWorks = await fetchApi('works');
+  apiDataCategories = await fetchApi('categories');
+}
+
+async function fetchApi(endPoint) {
+  const endPointName = endPoint;
+  const res = await fetch(api + endPointName);
   const data = await res.json();
 
   return data;
 }
 
-async function displayWorks() {
-  const apiDataWorks = await fetchWorks();
+function displayWorks(categoryId) {
+  const cards = document.querySelector('.gallery');
+  cards.innerHTML = '';
 
   for (let i = 0; i < apiDataWorks.length; i++) {
-    const titleWork = apiDataWorks[i].title;
-    const imgWork = apiDataWorks[i].imageUrl;
+    const currentWork = apiDataWorks[i];
 
-    const workDisplay = document.createElement('figure');
-    const workImg = document.createElement('img');
-    const workTitle = document.createElement('figcaption');
+    if (!categoryId || currentWork.categoryId === categoryId) {
+      const titleWork = currentWork.title;
+      const imgWork = currentWork.imageUrl;
 
-    workImg.src = imgWork;
-    workImg.alt = titleWork;
-    workTitle.textContent = titleWork;
+      const workDisplay = document.createElement('figure');
+      const workImg = document.createElement('img');
+      const workTitle = document.createElement('figcaption');
 
-    workDisplay.appendChild(workImg);
-    workDisplay.appendChild(workTitle);
+      workImg.src = imgWork;
+      workImg.alt = titleWork;
+      workTitle.textContent = titleWork;
 
-    const cards = document.querySelector('.gallery');
-    cards.appendChild(workDisplay);
+      workDisplay.appendChild(workImg);
+      workDisplay.appendChild(workTitle);
+
+      cards.appendChild(workDisplay);
+    }
   }
 }
 
-displayWorks();
+function displayButtons() {
+  const filterButtons = document.querySelector('#filter');
+
+  const allCategory = document.createElement('li');
+  allCategory.innerText = 'Tous';
+  allCategory.addEventListener('click', () => {
+    displayWorks();
+    toggleFilterSelected(allCategory);
+  });
+
+  filterButtons.appendChild(allCategory).classList.add('filter-selected');
+
+  for (let i = 0; i < apiDataCategories.length; i++) {
+    const categoryId = apiDataCategories[i].id;
+    const categoryName = apiDataCategories[i].name;
+
+    const nameCategory = document.createElement('li');
+    nameCategory.innerText = categoryName;
+    nameCategory.addEventListener('click', () => {
+      displayWorks(categoryId);
+      toggleFilterSelected(nameCategory);
+    });
+
+    filterButtons.appendChild(nameCategory);
+  }
+}
+
+function toggleFilterSelected(element) {
+  const filterButtons = document.querySelectorAll('#filter li');
+  filterButtons.forEach((button) => button.classList.remove('filter-selected'));
+  element.classList.add('filter-selected');
+}
+
+async function init() {
+  await fetchData();
+  displayWorks();
+  displayButtons();
+}
+
+init();
