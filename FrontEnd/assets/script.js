c6159a2072b6a0cb0687e478ee65fb2d55ec44c9
Tony THERLIER <146343369+Erilerth@users.noreply.github.com>
2023-12-16 20:44:53 +0100

    Revert "refactor: refactor  + add login.js for login form"
    This reverts commit 486c4d7a2f2bf290de984e47e65867ad6f7a4e34.

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -161,6 +161,73 @@ async function init() {
 
 init();
 
+const form = document.querySelector('.login_form');
+if (form) {
+  const fields = ['E-mail', 'password'];
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let error = 0;
+
+    fields.forEach((field) => {
+      const input = document.querySelector(`#${field}`);
+      if (!validateFields(input)) {
+        error++;
+      }
+    });
+
+    if (error === 0) {
+      const user = {
+        email: document.querySelector('#E-mail').value,
+        password: document.querySelector('#password').value,
+      };
+
+      handleApiRequest(
+        'users/login',
+        'POST',
+        { 'Content-type': 'application/json' },
+        user
+      )
+        .then((data) => {
+          if (data.error) {
+            console.error('Error:', data.message);
+            document.querySelector('.error-message-all').style.display =
+              'block';
+            document.querySelector('.error-message-all').innerText =
+              'Votre E-mail ou votre Mot de passe est incorrect';
+          } else {
+            localStorage.setItem('user', JSON.stringify(data));
+            localStorage.setItem('auth', 1);
+            form.submit();
+          }
+        })
+        .catch((data) => {
+          console.error('error:', data.message);
+        });
+    }
+  });
+
+  function validateFields(field) {
+    if (field.value.trim() === '') {
+      setStatus(
+        field,
+        `${field.previousElementSibling.innerText} ne peut pas être vide`,
+        'error'
+      );
+      return false;
+    } else {
+      setStatus(field, null, 'success');
+      return true;
+    }
+  }
+
+  function setStatus(field, message, status) {
+    const errorMessage = field.nextElementSibling;
+    errorMessage.innerText = status === 'success' ? '' : message;
+    field.classList.toggle('input-error');
+  }
+}
+
 const logButton = document.querySelector('#login_logout');
 const auth = localStorage.getItem('auth');
 const isAuth = auth === '1';
